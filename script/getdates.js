f976eeb3e88885683f4ee42f01b3d03fddc37f89
jneberhard <jreberhard3@gmail.com>
2025-05-06 05:53:47 -0600

    home page

--- script/getdates.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
-
-
