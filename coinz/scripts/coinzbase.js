7208c1082ab47e8737e231e6feac008d00006dfb
jneberhard <jreberhard3@gmail.com>
2025-06-06 23:33:08 -0600

    coinz - header and footer

---
+++ coinz/scripts/coinzbase.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector("#menu");
+    const navMenu = document.querySelector("#navMenu");
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open");
+    navMenu.classList.toggle("open");
+  });
+
+  const currentPage = window.location.pathname.split("/").pop(); // get filename
+  const links = document.querySelectorAll("nav ul li a");
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
